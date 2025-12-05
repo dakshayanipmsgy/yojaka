8454504664de080a9312d3bdb1654d01db613588
dakshayanipmsgy <dakshayani.pmsgy@gmail.com>
2025-12-05 22:19:09 +0530

    Delete public directory

--- public/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/bootstrap.php';
-
-header('Location: ' . YOJAKA_BASE_URL . '/app.php?page=login');
-exit;
--- public/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/bootstrap.php';
-
-logout();
-
-header('Location: ' . YOJAKA_BASE_URL . '/login.php');
-exit;
